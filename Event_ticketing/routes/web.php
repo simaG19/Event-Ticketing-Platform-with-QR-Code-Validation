22eb152b3549ceaf57b8b80b10ff0eba0d63af82
simon <simonwubshet254@gmail.com>
2024-09-20 19:10:46 +0300

    setup

--- Event_ticketing/routes/web.php
+++ Event_ticketing/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\mukController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController; // Import EventController
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -39,17 +39,24 @@ Route::get('/organizer', function(){
     return view('organizer');
 })->name('organizer')->middleware('Event_organizer');
 
-
 Route::middleware(['auth', 'Customer'])->group(function(){
-  
+    Route::get('/user/events', [UserController::class, 'index'])->name('user.events');
+    Route::get('/user/events/{event}/tickets', [UserController::class, 'viewTickets'])->name('user.events.tickets');
+   // Route::get('/user/events/{event}/tickets', [UserController::class, 'viewTickets'])->name('user.events.tickets');
+
 });
+
 // Group event management routes for event organizers
 Route::middleware(['auth', 'Event_organizer'])->group(function () {
     Route::get('/organizer/events', [EventController::class, 'index'])->name('events.index');
     Route::get('/organizer/events/create', [EventController::class, 'create'])->name('events.create');
-    Route::post('/organizer/events', [EventController::class, 'store'])->name('events.store');
+    Route::post('/organizer/events/store', [EventController::class, 'store'])->name('events.store');
     Route::get('/organizer/events/{event}/tickets', [EventController::class, 'viewTickets'])->name('events.tickets')->middleware('Event_organizer');
     Route::get('/organizer/events/{id}/edit', [EventController::class, 'edit'])->name('events.edit');
     Route::put('/organizer/events/{id}', [EventController::class, 'update'])->name('events.update');
     Route::delete('/organizer/events/{id}', [EventController::class, 'destroy'])->name('events.destroy');
 });
+// Route::middleware(['auth', 'Customer'])->group(function() {
+//     Route::get('/user/events', [UserController::class, 'index'])->name('user.events');
+//     Route::get('/user/events/{event}/tickets', [UserController::class, 'viewTickets'])->name('user.events.tickets');
+// });
