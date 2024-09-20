d2e84dd78eb09aeab515e95eff1b0443f405258f
simon <simonwubshet254@gmail.com>
2024-09-20 14:15:59 +0300

    user and event model

---
+++ Event_ticketing/app/Models/Event.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $table='orgevents';
+
+    protected $fillable=[
+
+        'title',
+        'description',
+        
+        'start_time',
+        'end_time',
+        'ticket_price',
+        'total_tickets',
+        'tickets_sold',
+        'organizer_id',
+     
+    ];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+    ];
+
+}
